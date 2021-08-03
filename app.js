54435f6cc36ca7846bfb27cb640626bb6fdcdf5e
Aaryan <aryan.pal0ppq@gmail.com>
2021-08-03 21:51:21 +0530

    input-output processing done

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-prompt("Kindly enter something");
-alert()
+var btnTranslate  = document.querySelector("#btn-translate");
+var textArea = document.querySelector("#text-area");
+var outputDiv=document.querySelector("#output");
+
+// outputDiv.innerText='Input HTML';
+console.log(outputDiv);
+function clickHandler(){
+    console.log("clicked");
+    console.log("input",textArea.value)
+    }
+
+btnTranslate.addEventListener("click", clickHandler)
+
